7dbd9b2f69937c0409a3b5c6bcf9ca9cc2cbeb4b
SrijanReddy <srijanreddy309@gmail.com>
2021-11-28 00:50:42 -0600

    Update tree_sitter_parse.py

--- tree_sitter_parse.py
+++ tree_sitter_parse.py
@@ -7,6 +7,7 @@ import shutil
 link_of_repo=input('enter link: ')
 Repo.clone_from(link_of_repo,'clones');
 py_links,js_links,go_links,rby_links=[],[],[],[]
+links=[py_links,js_links,go_links,rby_links]
 fpath=glob.glob('clones/*')
 
 for i in fpath:
@@ -18,8 +19,23 @@ for i in fpath:
         go_links.append(i)
     if '.rb' in i:
         rby_links.append(i)
+
+otpt=input("what kind of files do you parse 1.python 2.js 3.go 4.ruby")        
+
 txt=[]
-for f in py_links:
+if otpt==1:
+    now_links=py_links
+if otpt==2:
+    now_links=js_links
+if otpt==3:
+    now_links=go_links   
+if otpt==4:
+    now_links=rby_links
+else:
+    print('invalid input')
+
+    
+for f in now_links:
     with open(f) as fle:
         try:
             lines = fle.read()
@@ -28,7 +44,7 @@ for f in py_links:
         except:
             print('cant open')
             pass
-
+      
 Language.build_library( 'build/my-languages.so',['tree-sitter-python','tree-sitter-ruby','tree-sitter-javascript','tree-sitter-go'])
 py_lang= Language('build/my-languages.so', 'python')
 pyth_parser=Parser()
@@ -45,3 +61,7 @@ rby_parser.set_language(ruby_lang)
 go_lang=Language('build/my-languages.so','go')
 goparser = Parser()
 goparser.set_language(go_lang)
+
+for i in txt:
+    lis=parser(i,)
+
